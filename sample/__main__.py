b0561f9bf0bf6a4f59dbb0274e9c4b231126e4bb
Ayesh Alshukri <a.alshukri@liverpool.ac.uk>
2018-07-30 14:55:00 +0100

    Added debug logging cmd options

--- sample/__main__.py
+++ sample/__main__.py
@@ -33,16 +33,20 @@ def init():
 	#
 	# Command line arguments
 	cmdparser = argparse.ArgumentParser()
+
+	# Debug
 	cmdparser.add_argument('-d', '--debug', dest='debug', action='store_true')
+	cmdparser.add_argument('debugfile', nargs='?', default=None)
+
 	# add option to have an argument for debug output to log file
-	
 	args = cmdparser.parse_args()
 	
 	if args.debug: 	
-		#logging.basicConfig(filename='example.log',level=logging.DEBUG)# --- log to file
-		logging.basicConfig(level=logging.DEBUG)			  			# --- log to screen
-	else: logging.basicConfig(level=logging.WARNING) # --- turn off debug logging		
+		if args.debugfile: logging.basicConfig(filename=args.debugfile,level=logging.DEBUG)  # --- log to file
+		logging.basicConfig(level=logging.DEBUG)			  		     # --- log to screen
+	else: logging.basicConfig(level=logging.WARNING) 					     # --- turn off debug	
 		
+
 #/init()	
 	
 if __name__ == '__main__':
